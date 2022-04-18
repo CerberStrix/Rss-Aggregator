82b2c3b702540c1a55a1e6b9728775e462a1af77
Alisher Kim <cerberstrix@gmail.com>
2022-04-18 10:07:13 +0500

    build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './index.html'
         }),
         new MiniCssExtractPlugin({
             filename: "styles.css",

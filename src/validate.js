642607663bda9f503c56444e837d1bdb7c48445d
Alisher Kim <cerberstrix@gmail.com>
2022-04-20 12:02:08 +0500

    some fix

--- src/validate.js
+++ src/validate.js
@@ -1,12 +1,13 @@
 import * as yup from 'yup';
 
 export default (link, state) => {
+  console.log('Валидируем');
   const feedsLinks = state.feeds.map(({ feedLink }) => feedLink);
   console.log(feedsLinks);
   const schema = yup.string().url().notOneOf(feedsLinks).required();
   return schema.validate(link, { abortEarly: false })
     .then(() => '').catch((e) => {
-      console.log(e.message);
+      console.log('Поймали ошибку на валидации');
       switch (e.message) {
         case 'this is a required field':
           throw new Error('emptyUrl');

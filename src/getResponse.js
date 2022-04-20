f55a2bdc63299a6ba1e55ced485f15dfef7881bb
Alisher Kim <cerberstrix@gmail.com>
2022-04-20 12:07:03 +0500

    some fix

--- src/getResponse.js
+++ src/getResponse.js
@@ -1,6 +1,9 @@
 export default (rss) => fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(rss)}`)
   .then((response) => {
-    if (response.ok) return response.json();
+    if (response.ok) {
+      return response.json();
+    }
+    console.log('Ошибка с запросом');
     throw new Error('netWorkError');
   })
   .then((data) => {

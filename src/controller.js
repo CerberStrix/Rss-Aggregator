d4583f230ac7dcf560712662cbb3306fd8af1963
Alisher Kim <cerberstrix@gmail.com>
2022-04-20 12:43:13 +0500

    some fix

--- src/controller.js
+++ src/controller.js
@@ -111,6 +111,8 @@ export default (state, watchedState) => {
       .catch((error) => {
         submitEl.disabled = false;
         formInput.removeAttribute('readonly');
+        formElement.reset();
+        formInput.focus();
         watched.rssForm.state = error.message;
         console.log(`Поймали ошибку ${error.message}`);
       });
--- src/getResponse.js
+++ src/getResponse.js
@@ -1,13 +1,13 @@
-export default (rss) => fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(rss)}`)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    console.log('Ошибка с запросом');
-    throw new Error('netWorkError');
-  })
-  .then((data) => {
-    console.log('Запрос прошел');
-    const xmlString = data.contents;
-    return xmlString;
-  });
+import axios from 'axios';
+
+export default (rss) => {
+  const parsedURL = new URL('https://allorigins.hexlet.app/get');
+  parsedURL.searchParams.set('disableCache', 'true');
+  parsedURL.searchParams.set('url', rss);
+  return axios.get(parsedURL.href)
+    .then((response) => response.data.contents)
+    .catch(() => {
+      console.log('Ошибка с запросом');
+      throw new Error('netWorkError');
+    });
+};
